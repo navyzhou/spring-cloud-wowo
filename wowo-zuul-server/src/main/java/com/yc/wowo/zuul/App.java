67e4d3bf9c1579a840e338b5a9890c9e7048e094
navyzhou <qfxsxhfy@126.com>
2021-03-06 09:57:15 +0800

    分服务、分模块

---
+++ wowo-zuul-server/src/main/java/com/yc/wowo/zuul/App.java
@@ -0,0 +1,13 @@
+package com.yc.wowo.zuul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
